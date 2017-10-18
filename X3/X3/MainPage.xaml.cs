bc852d3c40ae2dc39c0f405149d294a9c8c30e3f
Lixin Yang <lsrob@hotmail.com>
2017-10-18 23:41:27 +1100

    Add project files.

---
+++ X3/X3/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using X3.Views;
+using Xamarin.Forms;
+
+namespace X3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Page1Button_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
